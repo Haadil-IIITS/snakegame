4ba8cfed34305b0375c7f0d15e6b67e75a074e46
Haadil Basha <haadilbasha@gmail.com>
2026-01-05 16:55:28 +0530

    Implement high score feature in scoreboard
    Add high score tracking to the scoreboard.

--- scoreboard.py
+++ scoreboard.py
@@ -9,11 +9,20 @@ class Scoreboard(Turtle):
         self.score=0
         self.hideturtle()
         self.color("white")
+        self.highscore=0
         self.writing()
+
+
+    def reset_scoreboard(self):
+        if self.score > self.highscore:
+            self.highscore=self.score
+            self.score=0
+        self.writing()
+
 
     def writing(self):
         self.clear()
-        self.write(f"Your score is {self.score}", False, "center", font=("Frank", 14, "normal"))
+        self.write(f"Your score {self.score} Highest score {self.highscore} ", False, "center", font=("Frank", 14, "normal"))
 
 
     def increase(self):
